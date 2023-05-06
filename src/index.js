44a44fcbe2411752cd500dfaf8db611962bfff45
Josh Marshall <cjoshmarshall@gmail.com>
2023-05-06 20:00:20 +0530

    Body-Parser Fix, Nodemon Dev Fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+// const path = require("path");
 
 const app = express();
 dotenv.config();
 
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.set("strictQuery", true);
 mongoose
   .connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
@@ -17,17 +24,12 @@ mongoose
     console.log(err);
   });
 
-app.use(express.json());
-app.use(cors());
-
 app.use("/api/auth", require("./routes/authRoute"));
 app.use("/api/users", require("./routes/userRoute"));
 app.use("/api/categories", require("./routes/categoryRoute"));
 app.use("/api/guitars", require("./routes/productRoute"));
 app.use("/api/orders", require("./routes/orderRoute"));
 app.use("/api/newsletter", require("./routes/newsletterRoute"));
-
-const path = require("path");
 
 // if (process.env.NODE_ENV === "production") {
 //   app.use("/", express.static("client/build"));
